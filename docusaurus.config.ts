ce27576fe3b2b2433cb760b60655bf320b9f1548
Yatin <yatinuppal03@gmail.com>
2024-09-09 13:40:55 +0530

    Fix tailwind issue

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import tailwindPlugin from './plugins/tailwind-config.cjs';
 
 const config: Config = {
   title: 'Instasdr-Website',
@@ -28,6 +29,7 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   }, 
+  plugins: [tailwindPlugin], 
   presets: [
     [
       'classic',
